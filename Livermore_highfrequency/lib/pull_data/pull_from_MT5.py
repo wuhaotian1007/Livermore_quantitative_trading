2d47594a59ad7fcb449554ec4563e6272637683a
wuhaotian <wuhaotian1007@gmail.com>
2021-10-16 23:38:30 +0800

    update

--- Livermore_highfrequency/lib/pull_data/pull_from_MT5.py
+++ Livermore_highfrequency/lib/pull_data/pull_from_MT5.py
@@ -16,11 +16,10 @@ import MetaTrader5 as mt5
 import pandas as pd
 import sqlite3
 import time
-import datetime
 
 
-class pull_from_MT5_history:
-    def __init__(self, XAUUSD_data_path):
+class pull_from_MT5_M1:
+    def __init__(self, XAUUSD_data_path, length):
 
         # number of columns to be displayed
         pd.set_option('display.max_columns', 500)
@@ -32,7 +31,9 @@ class pull_from_MT5_history:
             quit()
 
         # 从当前开始拉取M1数据
-        rates = mt5.copy_rates_from_pos("XAUUSD", mt5.TIMEFRAME_M1, 0, 90000)
+        rates = mt5.copy_rates_from_pos("XAUUSD", mt5.TIMEFRAME_M1, 0, length)
+
+        # shut down connection to the MetaTrader 5 terminal
         mt5.shutdown()
 
         # create DataFrame out of the obtained data
@@ -44,49 +45,40 @@ class pull_from_MT5_history:
         rates_frame.to_csv(XAUUSD_data_path)
         print("MT5数据拉取成功,保存至{0}".format(XAUUSD_data_path))
 
-class XAUUSD_std:
-    def __init__(self, XAUUSD_data_path, XAUUSD_db_path) -> None:
-        # 连接数据库data.db和指针
-        conn = sqlite3.connect(XAUUSD_db_path)
-        c = conn.cursor()
 
-        # 创建表data_list收集
-        c.execute('''CREATE TABLE IF NOT EXISTS XAUUSD_list
-                (DATE           TEXT           PRIMARY KEY,
-                NAME            TEXT           NOT NULL,
-                CLOSE           UNSIGNED INT   NOT NULL,
-                TIMESTICKER     UNSIGNED INT   NOT NULL,
-                HIGH            UNSIGNED INT   NOT NULL,
-                LOW             UNSIGNED INT   NOT NULL,
-                VOLUME          UNSIGNED INT   DEFAULT 0,
-                NOTES           TEXT           NULL);''')
+class pull_from_MT5_H1:
+    def __init__(self, XAUUSD_data_path):
+        # number of columns to be displayed
+        pd.set_option('display.max_columns', 500)
+        pd.set_option('display.width', 1500)      # max table width to display
 
-        # 按路径读取数据
-        with open(XAUUSD_data_path, 'r', encoding="UTF-8") as f_XAUUSD:
-            XAUUSD_csv = csv.reader(f_XAUUSD)
-            # 跳过第一行
-            next(f_XAUUSD)
-            for row in XAUUSD_csv:
-                date = row[1]
-                # 改变日期格式从yyyy/m/d到yyyy-mm-dd
-                date = date.replace('/', '-')
-                date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-                date = date.strftime("%Y-%m-%d %H:%M:%S")
-                ts = time.mktime(time.strptime(date, "%Y-%m-%d %H:%M:%S"))
-                high = row[3]
-                low = row[4]
-                close = row[5]
-                volume = row[6]
-                # 插入数据
-                close = round(float(close), 2)
-                c.execute("insert into XAUUSD_list( DATE, NAME, TIMESTICKER, CLOSE, VOLUME, HIGH, LOW) \
-                            VALUES(?, ?, ?, ?, ?, ? , ?) ", (date, "XAUUSD", ts, close, volume, high, low))
+        # 建立与MetaTrader 5程序端的连接
+        if not mt5.initialize():
+            print("initialize() failed, error code =", mt5.last_error())
+            quit()
 
-        # 保存并关闭数据库连接
-        conn.commit()
-        conn.close()
-        print("XAUUSD_Daily标准化入库成功")
-        
+        # set time zone to UTC
+        timezone = pytz.timezone("Etc/UTC")
+        # create 'datetime' objects in UTC time zone to avoid the implementation of a local time zone offset
+        utc_from = datetime(2020, 1, 1, tzinfo=timezone)
+        utc_to = datetime.now() - timedelta(days=1)
+        # get bars from USDJPY M5 within the interval of 2020.01.10 00:00 - 2020.01.11 13:00 in UTC time zone
+        rates = mt5.copy_rates_range(
+            "XAUUSD", mt5.TIMEFRAME_H1, utc_from, utc_to)
+
+        # shut down connection to the MetaTrader 5 terminal
+        mt5.shutdown()
+
+        # create DataFrame out of the obtained data
+        rates_frame = pd.DataFrame(rates)
+        # convert time in seconds into the 'datetime' format
+        rates_frame['time'] = pd.to_datetime(rates_frame['time'], unit='s')
+
+        # pandas数据可视化
+        rates_frame.to_csv(XAUUSD_data_path)
+        print("MT5数据拉取成功,保存至{0}".format(XAUUSD_data_path))
+
+
 if __name__ == "__main__":
-    pull_from_MT5_history(r"raw_data\XAUUSD_M1.csv")
-    XAUUSD_std(r"raw_data\XAUUSD_M1.csv", r"database\XAUUSD.db")
+    pull_from_MT5_M1(r"raw_data\XAUUSD_M1.csv", 90000)
+    pull_from_MT5_H1(r"raw_data\XAUUSD_H1.csv")
--- Livermore_highfrequency/lib/pull_data/pull_from_yfinance.py
+++ Livermore_highfrequency/lib/pull_data/pull_from_yfinance.py
@@ -20,71 +20,35 @@ import yfinance as yf
 
 
 class pull_from_yfinance:
-    def __init__(self, DXY_data_path, TNX_data_path, raw_data_db_path):
+    def __init__(self, DXY_data_path, TNX_data_path, frequency, length):
 
         # yfinance拉取数据
-        df_TNX = yf.download("^TNX", interval="2m", period="60d")
-        df_DXY = yf.download("DX-Y.NYB", interval="2m", period="60d")
+        df_TNX = yf.download("^TNX", interval=frequency, period=length)
+        df_DXY = yf.download("DX-Y.NYB", interval=frequency, period=length)
 
         # pandas数据可视化
         df_TNX.to_csv(TNX_data_path)
         df_DXY.to_csv(DXY_data_path)
 
-        # 连接数据库data.db和指针
-        conn = sqlite3.connect(raw_data_db_path)
-        c = conn.cursor()
+        print("yfinance:{0}数据{1}拉取成功".format(frequency, length))
 
-        # 创建表data_list收集
-        c.execute('''CREATE TABLE IF NOT EXISTS DXY_list
-            (DATE            TEXT           PRIMARY KEY,
-                NAME            TEXT           NOT NULL,
-                CLOSE           UNSIGNED INT   NOT NULL,
-                TIMESTICKER     UNSIGNED INT   NOT NULL,
-                VOLUME          UNSIGNED INT   DEFAULT 0);''')
 
-        # 按路径读取数据
+class pull_from_yfinance_H1:
+    def __init__(self, DXY_data_path, TNX_data_path):
+        # 数据范围开始日期
+        start_date = "2020-01-01"
+        # yfinance拉取数据
+        df_TNX = yf.download("^TNX", interval="1h", start=start_date)
+        df_DXY = yf.download("DX-Y.NYB", interval="1h", start=start_date)
 
-        with open(DXY_data_path, 'r', encoding="UTF-8") as f_DXY:
-            DXY_csv = csv.reader(f_DXY)
-            # 跳过第一行
-            next(DXY_csv)
-            for row in DXY_csv:
-                date = row[0][0:19]
-                close = row[5]
-                close = round(float(close), 2)
-                ts = time.mktime(time.strptime(date, "%Y-%m-%d %H:%M:%S"))
-                c.execute("insert into DXY_list(DATE, NAME, TIMESTICKER, CLOSE) \
-                            VALUES(?, ?, ?, ?) ", (date, "DXY", ts, close))
+        # pandas数据可视化
+        df_TNX.to_csv(TNX_data_path)
+        df_DXY.to_csv(DXY_data_path)
 
-        # 创建表data_list收集
-        c.execute('''CREATE TABLE IF NOT EXISTS TNX_list
-            (DATE            TEXT           PRIMARY KEY,
-                NAME            TEXT           NOT NULL,
-                CLOSE           UNSIGNED INT   NOT NULL,
-                TIMESTICKER     UNSIGNED INT   NOT NULL,
-                VOLUME          UNSIGNED INT   DEFAULT 0);''')
-
-        # 按路径读取数据
-
-        with open(TNX_data_path, 'r', encoding="UTF-8") as f_TNX:
-            TNX_csv = csv.reader(f_TNX)
-            # 跳过第一行
-            next(TNX_csv)
-            for row in TNX_csv:
-                date = row[0][0:19]
-                close = row[5]
-                close = round(float(close), 2)
-                ts = time.mktime(time.strptime(date, "%Y-%m-%d %H:%M:%S"))
-                c.execute("insert into TNX_list( DATE, NAME, TIMESTICKER, CLOSE) \
-                            VALUES(?, ?, ?, ?) ", (date, "TNX", ts, close))
-
-        # 关闭数据库连接
-        conn.commit()
-        conn.close()
-
-        print("yfinance数据拉取成功")
+        print("yfinanceH1数据拉取成功")
 
 
 if __name__ == "__main__":
-    pull_from_yfinance(r"raw_data\DXY.csv",
-                       r"raw_data\TNX.csv", r"database\XAUUSD.db")
+    pull_from_yfinance(r"raw_data\DXY_M2.csv",
+                       r"raw_data\TNX_M2.csv", "2m", "60d")
+    pull_from_yfinance_H1(r"raw_data\DXY_H1.csv", r"raw_data\TNX_H1.csv")
